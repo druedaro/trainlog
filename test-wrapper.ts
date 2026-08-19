ed6881994a0cf9b114acd14108b4e180bc74bca8
David <ruedarosasdavid@gmail.com>
2026-08-19 15:27:00 +0200

    chore: cleanup temporary backend scripts

--- test-wrapper.ts
+++
@@ -1,4 +0,0 @@
-import handler from './api/generateDiscover.ts';
-
-// We need to bypass the Auth check inside the handler.
-// Actually, since it imports './lib/verifyToken.js', I can create a mock!
